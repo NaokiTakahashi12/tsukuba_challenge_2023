64ee6959483f1013589aabef65a80136082119f3
Naoki Takahashi <naoki.takahashi060@gmail.com>
2023-10-23 22:22:45 +0900

    Organized sensor launch

--- tc23_bringup/launch/sensors.launch.py
+++ tc23_bringup/launch/sensors.launch.py
@@ -29,81 +29,29 @@ import launch_ros
 
 
 def generate_launch_description():
-    velodyne_pointcloud_share_dir = get_package_share_directory('velodyne_pointcloud')
-    velodyne_transform_file = os.path.join(
-        velodyne_pointcloud_share_dir,
-        'config',
-        'VLP32C-velodyne_transform_node-params.yaml'
-    )
-    with open(velodyne_transform_file, 'r') as f:
-        converted_velodyne_transform_param \
-            = yaml.safe_load(f)['velodyne_transform_node']['ros__parameters']
-    converted_velodyne_transform_param['calibration'] = os.path.join(
-        velodyne_pointcloud_share_dir,
-        'params',
-        'VeloView-VLP-32C.yaml'
+    this_pkg_share_dir = get_package_share_directory('tc23_bringup')
+    this_pkg_include_launch_dir = os.path.join(
+        this_pkg_share_dir,
+        'launch',
+        'includes'
     )
     return launch.LaunchDescription([
         launch.actions.GroupAction([
             launch.actions.IncludeLaunchDescription(
                 launch.launch_description_sources.AnyLaunchDescriptionSource(
                     os.path.join(
-                        get_package_share_directory('realsense2_camera'),
-                        'launch',
-                        'rs_launch.py'
+                        this_pkg_include_launch_dir,
+                        'd455.launch.py'
                     )
-                ),
-                launch_arguments={
-                    'camera_name': 'd455',
-                    'enable_depth': 'true',
-                    'enable_infra': 'true',
-                    'enable_infra1': 'true',
-                    'enable_infra2': 'true',
-                    'depth_module.exposure': '8500',
-                    'depth_module.gain': '16',
-                    'depth_module.hdr_enabled': 'false',
-                    'enable_sync': 'true',
-                    'enable_rgbd': 'true',
-                    'enable_gyro': 'true',
-                    'enable_accel': 'true',
-                    'unite_imu_method': '1',
-                    'align_depth.enable': 'true',
-                    'decimation_filter.enable': 'true',
-                    'spatial_filter.enable': 'true',
-                    'temporal_filter.enable': 'true',
-                    'hdr_merge.enable': 'false'
-                }.items()
+                )
             ),
-            launch_ros.actions.Node(
-                package='velodyne_driver',
-                executable='velodyne_driver_node',
-                output='screen',
-                parameters=[
+            launch.actions.IncludeLaunchDescription(
+                launch.launch_description_sources.AnyLaunchDescriptionSource(
                     os.path.join(
-                        get_package_share_directory('velodyne_driver'),
-                        'config',
-                        'VLP32C-velodyne_driver_node-params.yaml'
-                    ),
-                    {'device_ip': '192.168.123.201'}
-                ]
-            ),
-            launch_ros.actions.Node(
-                package='velodyne_pointcloud',
-                executable='velodyne_transform_node',
-                output='screen',
-                parameters=[converted_velodyne_transform_param]
-            ),
-            launch_ros.actions.Node(
-                package='velodyne_laserscan',
-                executable='velodyne_laserscan_node',
-                output='screen',
-                parameters=[
-                    os.path.join(
-                        get_package_share_directory('velodyne_laserscan'),
-                        'config',
-                        'default-velodyne_laserscan_node-params.yaml'
+                        this_pkg_include_launch_dir,
+                        'vlp32c.launch.py'
                     )
-                ]
-            )
+                )
+            ),
         ])
     ])
